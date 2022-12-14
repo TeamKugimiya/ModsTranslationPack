5dd428f3abdbe5891626d2c907d7b2a51ad7c703
xMikux <26039249+xMikux@users.noreply.github.com>
2022-12-15 03:31:24 +0800

    ci: 稍微更改一下 if else

--- .github/scripts/version_change.sh
+++ .github/scripts/version_change.sh
@@ -2,23 +2,31 @@
 
 cd "$GITHUB_WORKSPACE" || exit
 
-if [ "$1" = "1" ];then
-  sed -i "s/GitVersion/git ${GITHUB_SHA::7}/1" pack.mcmeta
-  echo "Add git version"
-elif [ "$1" = "2" ];then
-  sed -i "s/6GitVersion/b$2/1" pack.mcmeta
-  echo "Add release tag version"
-elif [ "$1" = "3" ];then
-  if [ "$2" = "1.18.x" ];then 
-    sed -i 's/9/8/1' pack.mcmeta
-    echo "Change to 1.18.x format"
-  elif [ "$2" = "1.19.x" ];then
-    echo "1.19.x, no change format"
-  else
+module_mode=$1
+version=$2
+
+case $module_mode in
+  "1")
+    sed -i "s/GitVersion/git ${GITHUB_SHA::7}/1" pack.mcmeta
+    echo "Add git version"
+    ;;
+  "2")
+    sed -i "s/6GitVersion/b$2/1" pack.mcmeta
+    echo "Add release tag version"
+    ;;
+  "3")
+    if [ "$version" = "1.18.x" ];then 
+      sed -i 's/9/8/1' pack.mcmeta
+      echo "Change to 1.18.x format"
+    elif [ "$version" = "1.19.x" ];then
+      echo "1.19.x, no change format"
+    else
+      echo "Error, no value found..."
+      exit 1
+    fi
+    ;;
+  *)
     echo "Error, no value found..."
-    exit 1
-  fi
-else
-  echo "Error, no value found..."
-  exit 1
-fi
+    exit 128
+    ;;
+esac
