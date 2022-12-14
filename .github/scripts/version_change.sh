c0ca5acd522b51aec7137ccc03a8348f4a298edd
xMikux <26039249+xMikux@users.noreply.github.com>
2022-12-15 03:40:09 +0800

    ci: 再次修改，並順便提升資源包版本至 12 (1.19.3)

--- .github/scripts/version_change.sh
+++ .github/scripts/version_change.sh
@@ -4,6 +4,7 @@ cd "$GITHUB_WORKSPACE" || exit
 
 module_mode=$1
 version=$2
+default_version=12
 
 case $module_mode in
   "1")
@@ -11,12 +12,12 @@ case $module_mode in
     echo "Add git version"
     ;;
   "2")
-    sed -i "s/6GitVersion/b$2/1" pack.mcmeta
+    sed -i "s/6GitVersion/b$version/1" pack.mcmeta
     echo "Add release tag version"
     ;;
   "3")
     if [ "$version" = "1.18.x" ];then 
-      sed -i 's/9/8/1' pack.mcmeta
+      sed -i "s/$default_version/8/1" pack.mcmeta
       echo "Change to 1.18.x format"
     elif [ "$version" = "1.19.x" ];then
       echo "1.19.x, no change format"
