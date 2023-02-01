520112ab72811d4e057e9fc45d2bd5467152bbf8
xMikux <26039249+xMikux@users.noreply.github.com>
2023-02-01 13:12:29 +0800

    ci(Verify): 更改驗證

--- .github/scripts/verify_folder.sh
+++ .github/scripts/verify_folder.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-find assets/* -prune -type d | while IFS= read -r d; do 
-  if [ -f "$d/lang/zh_tw.json" ]; then
-    name=$(echo "$d" | sed 's+assets/++' | sed 's+/lang/++')
-    echo "$name 翻譯檔案驗證通過！"
+# Array assets folder
+
+list=(assets/*)
+
+# Check langauge have original langauge file & trnalsate
+
+for i in "${list[@]}"; do
+  if [ -f "$i/lang/en_us.json" ] && [ -f "$i/lang/zh_tw.json" ]; then
+    # shellcheck disable=SC2001
+    name=$(echo "$i" | sed 's+assets/++')
+    json-diff -k "$i/lang/en_us.json" "$i/lang/zh_tw.json"
+    echo "$name 模組語言驗證成功！"
   else
-    name=$(echo "$d" | sed 's+assets/++' | sed 's+/lang/++')
-    echo "::error ::錯誤！ $name 模組並未包含翻譯檔案"
-    exit 1
+    # shellcheck disable=SC2001
+    name=$(echo "$i" | sed 's+assets/++')
+    echo "::error ::❎ 錯誤！$name 模組並未包含原始或翻譯語言檔。"
+    # exit 1
   fi
 done
