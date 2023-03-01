8fbe76c6576e5fcf112105d0eeb69d9861656a46
xMikux <26039249+xMikux@users.noreply.github.com>
2023-03-01 21:25:11 +0800

    ci: 補齊忘記複製的東西

--- .github/scripts/multiversions.sh
+++ .github/scripts/multiversions.sh
@@ -18,6 +18,7 @@ setup_pack () {
   echo "初始化翻譯包資料夾"
   mkdir -p pack/assets
   command_excuter "cp -r $multiversion_path/configs/* $pack_path" "成功移動設定！" "移動設定時發生錯誤！"
+  command_excuter "cp -r LICENSE $pack_path" "成功移動授權條款！" "移動授權條款時發生錯誤！"
   command_excuter "cp -r $multiversion_path/Forge/main/* $pack_path/assets" "成功移動主版本！" "移動主版本時發生錯誤！"
   command_excuter "cp -r $multiversion_path/Patcher/* $pack_path/assets" "成功合併 Patcher！" "合併 Patcher 時發生錯誤！"
   echo "移動授權條款相關檔案"
