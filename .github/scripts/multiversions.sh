2aca7acc9afbc0b19b18718522a3c73c1b0b0791
xMikux <26039249+xMikux@users.noreply.github.com>
2023-03-01 20:05:52 +0800

    ci: 調整 override

--- .github/scripts/multiversions.sh
+++ .github/scripts/multiversions.sh
@@ -15,10 +15,13 @@ pack_path=$workflow_path/pack
 
 ## Setup pack
 setup_pack () {
+  echo "初始化翻譯包資料夾"
   mkdir -p pack/assets
   command_excuter "cp -r $multiversion_path/configs/* $pack_path" "成功移動設定！" "移動設定時發生錯誤！"
   command_excuter "cp -r $multiversion_path/Forge/main/* $pack_path/assets" "成功移動主版本！" "移動主版本時發生錯誤！"
   command_excuter "cp -r $multiversion_path/Patcher/* $pack_path/assets" "成功合併 Patcher！" "合併 Patcher 時發生錯誤！"
+  echo "移動授權條款相關檔案"
+  command_excuter "mv $multiversion_path/Override/LICENSE_* $pack_path" "成功移動授權條款資料！" "移動授權條款資料時發生錯誤！"
 }
 
 # Mixer
@@ -56,8 +59,10 @@ multiversion () {
     local pack_path=$pack_path/assets
 
   if [ "$version" = "1.19.x" ]; then
+    mixer "Override" "main"
     mixer "Fabric" "main"
   elif [ "$version" = "1.18.x" ]; then
+    mixer "Override" "main"
     mixer "Fabric" "main"
     mixer "Fabric" "1.18"
     mixer "Forge" "1.18"
@@ -90,20 +95,12 @@ clean () {
   cleaning "Fabric" "1.18"
 }
 
-assets_mover () {
-  if [ -d "assets" ]; then
-    cp -r assets/* pack/assets
-    rm -r assets
-  fi
-}
-
 ## Main
 
 main () {
   clean
   setup_pack
   multiversion "$version"
-  assets_mover
 }
 
 main
