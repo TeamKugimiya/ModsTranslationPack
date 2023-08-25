32324e9717d35cc3efa59998e65f1ac52ca0712a
Miku <26039249+xMikux@users.noreply.github.com>
2023-08-25 12:35:57 +0000

    ci: 重新命名腳本

--- .github/scripts/content_replacer.py
+++ .github/scripts/content_replacer.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-def insert_content(file_path, start_tag, end_tag, content_insert):
+def insert_content(file_path: str, multiline_mode: bool, start_tag: str, end_tag: str, content_insert: str):  # noqa: E501
     with open(file_path, "r") as file:
         file_contents = file.read()
     
@@ -12,26 +12,34 @@ def insert_content(file_path, start_tag, end_tag, content_insert):
         print("未找到 Tags")
         sys.exit(1)
 
-    updated_contents = (
-        file_contents[:start_index + len(start_tag)] +
-        "\n" +
-        content_insert +
-        "\n" +
-        file_contents[end_index:]
-    )
+    if multiline_mode:
+        updated_contents = (
+            file_contents[:start_index + len(start_tag)] +
+            "\n" +
+            content_insert +
+            "\n" +
+            file_contents[end_index:]
+        )
+    else:
+        updated_contents = (
+            file_contents[:start_index + len(start_tag)] +
+            content_insert +
+            file_contents[end_index:]
+        )
 
     with open(file_path, "w") as file:
         file.write(updated_contents)
 
 def main():
     file_path = os.environ.get("FILE_PATH")
+    multiline_mode = os.environ.get("MULTILINE_MODE", False)
     start_tag = os.environ.get("START_TAG")
     end_tag = os.environ.get("END_TAG")
     content = os.environ.get("CONTENT")
     envs = [file_path, start_tag, end_tag, content]
 
     if all(envs):
-        insert_content(file_path, start_tag, end_tag, content)
+        insert_content(file_path, multiline_mode, start_tag, end_tag, content)
     else:
         print("錯誤，有環境變數未存在！")
         sys.exit(1)
