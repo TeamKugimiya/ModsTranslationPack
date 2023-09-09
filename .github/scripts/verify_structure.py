42c85117b7ca0059087437712582c40cdf16f5bd
Miku <26039249+xMikux@users.noreply.github.com>
2023-09-09 20:33:50 +0800

    chore(Structure CI): 小小整理與修正錯誤例外 (#402)
    歷史提交：
    * chore(Structure CI): 小小整理與修正錯誤例外
    * chore: 簡化格式
    * ci: 調整輸出訊息的方式
    * test: 進行測試
    * ci: 調整
    * ci: 完成

--- .github/scripts/verify_structure.py
+++ .github/scripts/verify_structure.py
@@ -1,101 +1,175 @@
+# pylint: disable=C0114
 import os
 import sys
 import json
-import logging
 from pathlib import Path
 
-if os.environ.get("CI"):
-    formatter = "%(levelname)s - %(message)s"
-    logging.basicConfig(level=logging.INFO, format=formatter)
-else:
-    formatter = "%(levelname)s - %(message)s"
-    logging.basicConfig(level=logging.INFO, format=formatter)
+# Message set list
+MSG_INFO = set()
+MSG_WARN = set()
+MSG_ERROR = set()
 
-logger = logging.getLogger(__name__)
+# Message error count
+ERROR_NUM = 0
 
-ERROR_MSG = "結構錯誤！"
-LANG_ORIGINAL_FILES = ["en_us.json", "en_us.lang"]
-LANG_TRANSLATE_FILES = ["zh_tw.json", "zh_tw.lang"]
-
-def loadJsonFile(filePath: str):
+def load_json(path: Path):
+    """
+    Load and return json data
+    """
     try:
-        with open(filePath, "r", encoding="utf8") as f:
-            data = json.load(f)
+        with open(path, "r", encoding="utf8") as json_data:
+            data = json.load(json_data)
             return data
-    except (FileNotFoundError, json.JSONDecodeError) as e:
-        logger.error(f"讀取 JSON 檔案時發生錯誤: {e}")
+    except (FileNotFoundError, json.JSONDecodeError) as exc:
+        print(f"讀取 JSON 檔案時發生錯誤: {exc}")
         return None
 
-def log_error_exit(error_msg, path):
-    logger.error(error_msg)
-    logger.error(f"路徑：{path}")
-    sys.exit(1)
+def log_message(msg: str, path: Path, msg_type: str):
+    """
+    A simple log function to add all message into their list
 
-def find_subfolders(root_folder):
+    Type list: INFO, WARN, ERROR
+    """
+    prefix_warn = "::warning::" if os.environ.get("CI") else ""
+    prefix_error = "::error::" if os.environ.get("CI") else ""
+
+    match msg_type:
+        case "INFO":
+            MSG_INFO.add(f"{msg} - {path}")
+        case "WARN":
+            MSG_WARN.add(f"{prefix_warn}{msg} - {path}")
+        case "ERROR":
+            MSG_ERROR.add(f"{prefix_error}{msg} - {path}")
+            global ERROR_NUM # pylint: disable=W0603
+            ERROR_NUM += 1
+        case _:
+            print("⚠️ 類型錯誤！")
+            sys.exit(1)
+
+def print_block(msg_list: set, group_title: str):
+    """
+    Print message with group (GitHub Action)
+    """
+    print(f"::group::{group_title}")
+    for i in msg_list:
+        print(i)
+    print("::endgroup::")
+
+def find_subfolders(root_folder: Path):
+    """
+    Find all subfolder and return list
+    """
     subfolders = []
 
-    for dir in os.listdir(root_folder):
-        dir_path = Path(root_folder, dir)
-        if os.path.isdir(dir_path):
-            subfolders.append(dir_path)
+    for sub in root_folder.iterdir():
+        if sub.is_dir():
+            subfolders.append(sub)
+        elif sub.is_file():
+            log_message("📁 外部驗證｜存在不該存在的檔案或資料夾", sub, "ERROR")
+        else:
+            log_message("📁 外部驗證｜找不到任何檔案", sub, "ERROR")
 
     return subfolders
 
-def check_lang_exists(path):
-    existing_files = set(os.listdir(path))
-    original_files_exist = set(LANG_ORIGINAL_FILES) & existing_files
-    translate_files_exist = set(LANG_TRANSLATE_FILES) & existing_files
+def check_lang_exists(path: Path):
+    """
+    Check folder list, and see the correct file
+    """
+    lang_original_files = ["en_us.json", "en_us.lang"]
+    lang_translate_files = ["zh_tw.json", "zh_tw.lang"]
+    existing_files = set(x.name for x in path.iterdir() if x.is_file())
+    original_files_exist = set(lang_original_files) & existing_files
+    translate_files_exist = set(lang_translate_files) & existing_files
 
     return len(original_files_exist) > 0 and len(translate_files_exist) > 0
 
-def check_dir_has_data(dir):
-    return any(os.scandir(dir))
+def check_dir_has_data(path):
+    """
+    Return True if the folder has data
+    """
+    return any(os.scandir(path))
 
 def validate_language(subdir):
+    """
+    Verify langauge is exist
+    """
     if check_lang_exists(subdir):
-        logger.debug("結構語言驗證通過。")
-        logger.debug(f"路徑：{subdir}")
+        log_message("🌐 語言｜結構驗證通過", subdir, "INFO")
     else:
-        log_error_exit(f"{ERROR_MSG}未包括正確語言檔案。", subdir)
+        log_message("🌐 語言｜結構驗證失敗（未包含 Json 翻譯檔）", subdir, "ERROR")
 
 def validate_manual(subdir):
+    """
+    TODO need a better way to verify
+    Using file size to check the dir is no empty
+    """
     if check_dir_has_data(subdir):
-        logger.debug("結構手冊驗證通過。")
-        logger.debug(f"路徑：{subdir}")
+        log_message("📖 手冊｜結構驗證通過", subdir, "INFO")
     else:
-        log_error_exit(f"{ERROR_MSG}手冊資料夾中未有任何資料！", subdir)
+        log_message("📖 手冊｜結構驗證失敗（手冊資料夾無任何資料）", subdir, "ERROR")
 
-def verify_structure(dir):
-    subfolders = find_subfolders(dir)
+def verify_structure(path):
+    """
+    Verify mods lang and patchouli dir
+    """
+    subfolders = find_subfolders(path)
 
     if not subfolders:
-        log_error_exit(f"{ERROR_MSG}未包含任何資料夾。", dir)
+        log_message("🚧 結構｜不存在任何資料夾。", path, "ERROR")
 
     for subdir in subfolders:
         subdir_name = subdir.name
-        if "lang" in subdir_name:
+        if subdir_name == "lang":
             validate_language(subdir)
-        elif "patchouli_books" in subdir_name:
+        elif subdir_name == "patchouli_books":
             validate_manual(subdir)
         else:
-            log_error_exit(f"{ERROR_MSG}資料夾下存在未被設定的結構。", subdir)
+            log_message("🚧 結構｜資料夾下存在未被設定的結構。", subdir, "ERROR")
 
 def verify_loop(platform, version):
+    """
+    Loop different platform and version
+    """
     scan_path = Path("MultiVersions", platform, version)
-    if not os.path.isdir(scan_path):
-        logger.warning(f"警告！{scan_path} 資料夾路徑並不存在。")
+    if not scan_path.is_dir():
+        log_message("🚧 結構｜資料夾不存在", scan_path, "WARN")
         return
 
     subfolders = find_subfolders(scan_path)
 
-    for dir in subfolders:
-        verify_structure(dir)
+    for sub in subfolders:
+        verify_structure(sub)
 
-def main(versions_dict):
-    for platform in versions_dict["supported_platform"]:
-        for version in versions_dict["versions"]:
-            verify_loop(platform, version["dir_path"])
+def verify_clean(json_dict: dict):
+    """
+    Verify only json dict folder
+    """
+    path = Path("MultiVersions")
+    platform = json_dict["supported_platform"]
+    list_version = {version["dir_path"] for version in json_dict["versions"]}
+    allowed_items = set(platform + ["configs", "Patcher", "README.md"])
+    error_message = "📁 外部驗證｜存在不允許的檔案或資料夾"
+
+    for item in path.iterdir():
+        if item.name not in allowed_items:
+            log_message(error_message, item, "ERROR")
+
+        if item.name in platform:
+            for sub_item in item.iterdir():
+                if sub_item.name not in list_version:
+                    log_message(error_message, sub_item, "ERROR")
 
 if __name__ == "__main__":
-    versions_dict = loadJsonFile(".github/configs/versions.json")
-    main(versions_dict)
+    versions_dict = load_json(".github/configs/versions.json")
+    verify_clean(versions_dict)
+
+    for p in versions_dict["supported_platform"]:
+        for v in versions_dict["versions"]:
+            verify_loop(p, v["dir_path"])
+
+    print_block(MSG_INFO, "資訊")
+    print_block(MSG_WARN, "警告")
+    print_block(MSG_ERROR, "錯誤")
+
+    if ERROR_NUM > 0:
+        sys.exit(1)
